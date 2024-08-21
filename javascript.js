0f986e7943556a041322dc78982b8b3d37750d70
tmfrl99 <99esg@naver.com>
2024-08-21 13:08:28 +0900

    top 버튼 footer 위에 고정

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // top 버튼
   const topBtn = document.getElementById("top-btn");
+  const footer = document.querySelector("footer");
 
   // 초기 상태 설정
   topBtn.style.opacity = "0";
@@ -29,6 +30,24 @@ document.addEventListener("DOMContentLoaded", function () {
       behavior: "smooth",
     });
   });
+
+  function adjustButtonPosition() {
+    const footerRect = footer.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    const buttonHeight = topBtn.offsetHeight;
+    const imgHeight = document.querySelector('.background-img2').offsetHeight;
+
+    if (footerRect.top <= windowHeight - imgHeight + 40) {
+      topBtn.style.bottom = `${windowHeight - footerRect.top - imgHeight + buttonHeight + 40}px`;
+    } else {
+      topBtn.style.bottom = "40px";
+    }
+  }
+
+  // 스크롤 이벤트에 adjustButtonPosition 함수 바인딩
+  window.addEventListener("scroll", adjustButtonPosition);
+  // 윈도우 리사이즈 이벤트에도 바인딩 (화면 크기 변경 시에도 위치 조정)
+  window.addEventListener("resize", adjustButtonPosition);
 
   // map 출력
   const container = document.getElementById("map");
